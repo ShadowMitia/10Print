39fe099b3278d89d5c57ef3549b830d179923d60
Dimitri Belopopsky <dimitri@belopopsky.com>
2018-02-10 22:30:34 +0100

    Simplified random number generation

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,18 +1,10 @@
 #include "ofApp.h"
 
-double random_0_1()
-{
-  static std::random_device rd{};
-  static std::mt19937 generator{rd()};
-  std::uniform_real_distribution<double> dis(0.0, std::nextafter(1.0, std::numeric_limits<double>::max()));
-  return dis(generator);
-}
-
 
 //--------------------------------------------------------------
 void ofApp::setup(){
   ofBackground(ofColor::black);
-  ofSetFrameRate(5);
+  ofSetFrameRate(60);
   ofSetWindowTitle("10 PRINT");
 }
 
@@ -25,11 +17,14 @@ void ofApp::update(){
 void ofApp::draw(){
   ofSetColor(ofColor::white);
 
-  for (unsigned int i = 0; i < ofGetWindowWidth(); i += print_size)
+  for (int i = 0; i < ofGetWindowWidth(); i += print_size)
     {
-      for(unsigned int j = 0; j < ofGetWindowHeight(); j += print_size)
+      for(int j = 0; j < ofGetWindowHeight(); j += print_size)
 	{
-	  if (random_0_1() < 0.5)
+
+	  std::uniform_real_distribution<double> dis(0.0, std::nextafter(1.0, std::numeric_limits<double>::max()));
+
+	  if (dis(generator) < 0.5)
 	    {
 	      ofDrawLine(i, j + print_size, i + print_size, j);
 	    }
@@ -39,59 +34,4 @@ void ofApp::draw(){
 	    }
 	}
     }
-}
-
-//------------------------------------------------------------
-void ofApp::keyPressed(int key){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -4,7 +4,7 @@
 
 #include <random>
 
-constexpr const int print_size{50};
+constexpr int print_size{50};
 
 class ofApp : public ofBaseApp
 {
@@ -14,16 +14,7 @@ public:
   void update() override;
   void draw() override;
 
-  void keyPressed(int key) override;
-  void keyReleased(int key) override;
-  void mouseMoved(int x, int y ) override;
-  void mouseDragged(int x, int y, int button) override;
-  void mousePressed(int x, int y, int button) override;
-  void mouseReleased(int x, int y, int button) override;
-  void mouseEntered(int x, int y) override;
-  void mouseExited(int x, int y) override;
-  void windowResized(int w, int h) override;
-  void dragEvent(ofDragInfo dragInfo) override;
-  void gotMessage(ofMessage msg) override;
+  std::random_device rd{};
+  std::mt19937 generator{rd()};
 
 };
